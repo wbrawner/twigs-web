8fdc60b475cc874c70a5aeaca2fc7253aa39f7fa
William Brawner <me@wbrawner.com>
2020-07-09 18:19:21 -0700

    Fix date/time setting for transaction creation
    Signed-off-by: William Brawner <me@wbrawner.com>

--- src/app/transactions/add-edit-transaction/add-edit-transaction.component.ts
+++ src/app/transactions/add-edit-transaction/add-edit-transaction.component.ts
@@ -38,8 +38,8 @@ export class AddEditTransactionComponent implements OnInit, OnChanges {
       expense = true;
     }
     this.updateCategories(new MatRadioChange(undefined, expense));
-    this.transactionDate = d.toLocaleDateString(undefined, {year: 'numeric', month: '2-digit', day: '2-digit'});
-    this.currentTime = d.toLocaleTimeString(undefined, {hour: '2-digit', hour12: false, minute: '2-digit'});
+    this.transactionDate = `${d.getFullYear()}-${d.getMonth() + 1}-${d.getDate()}`
+    this.currentTime = d.toTimeString().slice(0, 5);
   }
 
   ngOnChanges(changes: SimpleChanges) {
