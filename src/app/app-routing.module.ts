1a5dce0f726436b59ef10f3b8fafa2aef3cd7233
William Brawner <me@wbrawner.com>
2024-03-03 10:21:00 -0700

    Bump Angular to v15

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -33,7 +33,7 @@ const routes: Routes = [
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(routes, { relativeLinkResolution: 'legacy' })
+    RouterModule.forRoot(routes, {})
   ],
   exports: [
     RouterModule
